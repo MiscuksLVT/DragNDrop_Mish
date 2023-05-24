c40556097fcb11a9261e871791a6de4742488a45
Daniils <daniils.miscuks@gmail.com>
2023-05-24 12:19:40 +0300

    Drag and drop Uzsakasana script

---
+++ DragAndDrop/Assets/Script/DragAndDrop.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragAndDrop : MonoBehaviour,IBeginDragHandler,IDragHandler,IEndDragHandler {
+    //Uzglat noradi uz objelkti skriptu\
+    public Objekti objektiSkripts;
+    //Uzglaba noradi uz katra objekta CanvasGroup
+    private CanvasGroup kanvasGrupa;
+    private RectTransform velkObjektutransf;
+
+    void Start()
+    {
+        kanvasGrupa = GetComponent<CanvasGroup>();
+        velkObjektutransf= GetComponent<RectTransform>();
+
+    }
+
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+
+    }
+
+  
+	
+	
+}
